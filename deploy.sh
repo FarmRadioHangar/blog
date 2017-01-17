328133f7ce3519012268afe88759a87531849d81
Surfacescan <sullivan.bart@gmail.com>
2017-01-17 20:02:46 +0300

    debugging

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@ git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
+pwd
 # Build the project.
 hugo -t casper -d public
 
 # Go To Public folder
 cd public
+pwd
 
 git status
 
